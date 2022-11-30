93b023896a59961534918d3706fd0d37479772ae
Aryan Raj <aryan.raj15604@gmail.com>
2022-11-30 16:35:03 +0530

    Updates

---
+++ precache-manifest.88e943089f4abb3914ce3191e5e22fc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e283f8270edbd14e38b23782228940e",
+    "url": "/IITBSkynetBot/index.html"
+  },
+  {
+    "revision": "ffbb329c33be0d6a8d82",
+    "url": "/IITBSkynetBot/static/css/main.b000155c.chunk.css"
+  },
+  {
+    "revision": "ec3069ff17d2bfafefa8",
+    "url": "/IITBSkynetBot/static/js/2.97d7b6a6.chunk.js"
+  },
+  {
+    "revision": "ffbb329c33be0d6a8d82",
+    "url": "/IITBSkynetBot/static/js/main.f2d3ec14.chunk.js"
+  },
+  {
+    "revision": "65d80967a71bb2bc4f20",
+    "url": "/IITBSkynetBot/static/js/runtime~main.92e60683.js"
+  }
+]);
